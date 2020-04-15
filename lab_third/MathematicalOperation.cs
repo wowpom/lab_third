78dd92e33ccdb6c887d26e4da6eb60a3951f2382
Valik0805 <56127421+Valik0805@users.noreply.github.com>
2020-04-15 15:18:24 +0700

    Добавьте файлы проекта.

---
+++ lab_third/MathematicalOperation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_third
+{
+    class MathematicalOperation
+    {
+        public double Add(double x)
+        {
+            x = x + 2;
+            Console.WriteLine("Операция сложения:" + x);
+            return x;
+        }
+        public double Multiply(double x)
+        {
+            x = x * 2;
+            Console.WriteLine("Операция умножения:" + x);
+            return x;
+        }
+        public double Subtraction(double x)
+        {
+            x = x - 1;
+            Console.WriteLine("Операция вычитания:" + x);
+            return x;
+        }
+        public double Division(double x)
+        {
+            x = x / 3;
+            Console.WriteLine("Операция деления без остатка:" + x);
+            return x;
+        }
+        public double Pow(double x)
+        {
+            x = x * x;
+            Console.WriteLine("Операция квадрата:" + x);
+            return x;
+        }
+    }
+}
---
+++ lab_third/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace lab_third
+{
+    class Program
+    {
+        private delegate double MyDelegate(double x);
+
+        static void Menu()
+        {
+            MyDelegate[] mydelegate = new MyDelegate[10];
+            Random rnd = new Random();
+            MathematicalOperation meth = new MathematicalOperation();
+            for (int i = 0; i < 10; i++)
+            {
+                int a = Convert.ToInt32(rnd.Next(1, 5));
+                switch (a)
+                {
+                    case 1:
+                        mydelegate[i] = meth.Add;
+                        break;
+                    case 2:
+                        mydelegate[i] = meth.Multiply;
+                        break;
+                    case 3:
+                        mydelegate[i] = meth.Division;
+                        break;
+                    case 4:
+                        mydelegate[i] = meth.Subtraction;
+                        break;
+                    case 5:
+                        mydelegate[i] = meth.Pow;
+                        break;
+
+                }
+            }
+            Console.WriteLine("Введите число");
+            double num = double.Parse(Console.ReadLine());
+            for (int j = 0; j < 10; j++)
+            {
+                num = mydelegate[j](num);
+
+            }
+            Console.WriteLine("Вы хотите ввести ещё одно число? \n Введите 1 если да");
+            if (Console.ReadLine() == "1")
+                Menu();
+        }
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+    }
+}
